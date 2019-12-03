23dd659efd9f92dbf39749bc56c6c27d91e59498
Michael Baaske <MBaaske@baytechconsulting.com>
2019-12-03 07:01:00 -0800

    completing details view

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,4 +1,7 @@
+import { EventsService } from './../events.service';
+import { EventResponse, EmergenyEvent, Acknowledgement } from './../interfaces';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
+  eventId: number;
+  eventResponse: EventResponse;
+  event: EmergenyEvent;
+  acknowledgments: Acknowledgement[] = [];
+  newNote = '';
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private eventService: EventsService
+  ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    // tslint:disable-next-line:no-string-literal
+    this.eventId = +this.route.snapshot.params['eventId'];
+    this.eventResponse = await this.eventService.getById(this.eventId).toPromise();
+    this.event = this.eventResponse.event;
+    this.acknowledgments = await this.eventService.getAcknowledgements(this.eventResponse).toPromise();
   }
 
 }
